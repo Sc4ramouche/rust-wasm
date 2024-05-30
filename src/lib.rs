2364d79dc6da863d31edea72750bfc94ba52e227
Vladislav Kovechenkov <vladislav.kovechenkov@gmail.com>
2024-05-30 22:49:36 +0200

    feat: call js func from wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add_one(x: i32) -> i32 {
-    x + 1
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hi, {}!", name));
 }
